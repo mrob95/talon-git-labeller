12f617e6f1534649f672d56081eca600e6b9f9d7
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2021-03-12 09:11:41 +0000

    Minor cleanup

--- branch.py
+++ branch.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from utils import sort_out_windows_colours, map_numbers_to_spoken, set_list, run_command
 import re
 import sys
@@ -9,28 +10,31 @@ GIT_STATUS_ITEMS_LIST    = "git_branch_items"
 if platform.system().lower() == "windows":
     sort_out_windows_colours()
 
-out = run_command(["git", "branch"])
-
 COLOUR_GREEN = "\x1b[32m"
 COLOUR_RESET = "\x1b[0m"
 
-branch_num = 1
-branch_map = {}
-colour = ""
+def git_branch() -> Dict[str, str]:
+    out = run_command(["git", "branch"])
 
-for line in out.rstrip().split("\n"):
-    if line.startswith("*"):
-        # Current branch
-        branch = re.match(r"\*\s(.+?)$", line).group(1).strip()
-        line_to_print = f"* {COLOUR_GREEN}{branch}{COLOUR_RESET}"
-    else:
-        branch = line.strip()
-        line_to_print = line
-    spoken = map_numbers_to_spoken(branch_num)
-    branch_map[spoken] = branch
-    print(f" {branch_num: >2}. {line_to_print}")
-    branch_num += 1
+    branch_num = 1
+    branch_map = {}
 
-sys.stdout.flush()
+    for line in out.rstrip().split("\n"):
+        if line.startswith("*"):
+            # Current branch
+            branch = re.match(r"\*\s(.+?)$", line).group(1).strip()
+            line_to_print = f"* {COLOUR_GREEN}{branch}{COLOUR_RESET}"
+        else:
+            branch = line.strip()
+            line_to_print = line
+        spoken = map_numbers_to_spoken(branch_num)
+        branch_map[spoken] = branch
+        print(f" {branch_num: >2}. {line_to_print}")
+        branch_num += 1
 
-set_list(GIT_STATUS_ITEMS_CONTEXT, GIT_STATUS_ITEMS_LIST, branch_map)
+    sys.stdout.flush()
+    return branch_map
+
+if __name__ == '__main__':
+    branch_map = git_branch()
+    set_list(GIT_STATUS_ITEMS_CONTEXT, GIT_STATUS_ITEMS_LIST, branch_map)
--- status.py
+++ status.py
@@ -1,8 +1,8 @@
+from typing import Dict
 from utils import sort_out_windows_colours, map_numbers_to_spoken, set_list, run_command
 import re
 import platform
 import sys
-
 
 GIT_STATUS_ITEMS_CONTEXT = "user.apps.terminal.git"
 GIT_STATUS_ITEMS_LIST    = "git_status_items"
@@ -10,42 +10,47 @@ GIT_STATUS_ITEMS_LIST    = "git_status_items"
 if platform.system().lower() == "windows":
     sort_out_windows_colours()
 
-out = run_command(["git", "status"])
-
 COLOUR_GREEN = "\x1b[32m"
 COLOUR_RED   = "\x1b[31m"
 COLOUR_RESET = "\x1b[0m"
-colour_map = {
+
+COLOUR_MAP = {
     "Changes to be committed:": COLOUR_GREEN,
     "Changes not staged for commit:": COLOUR_RED,
     "Untracked files:": COLOUR_RED,
     "Unmerged paths:": COLOUR_RED,
 }
 
-file_num = 1
-file_map = {}
-colour = ""
+def git_status() -> Dict[str, str]:
+    out = run_command(["git", "status"])
 
-for line in out.strip().split("\n"):
-    if line in colour_map:
-        colour = colour_map[line]
+    file_num = 1
+    file_map = {}
+    colour = ""
 
-    contains_file = re.match(r"^\s+[^(\s]", line)
-    if contains_file:
-        # first group is e.g. 'modified:' or 'deleted:'
-        file_name_match = re.match(r"^\s+([a-z\s]+:\s+)?(.+?)$", line)
-        if file_name_match:
-            file_name = file_name_match.group(2).strip("/")
-            spoken = map_numbers_to_spoken(file_num)
-            file_map[spoken] = file_name
-            print(f"  {file_num: >2}.   {colour}{line.strip()}{COLOUR_RESET}")
-            file_num += 1
+    for line in out.strip().split("\n"):
+        if line in COLOUR_MAP:
+            colour = COLOUR_MAP[line]
+
+        contains_file = re.match(r"^\s+[^(\s]", line)
+        if contains_file:
+            # first group is e.g. 'modified:' or 'deleted:'
+            file_name_match = re.match(r"^\s+([a-z\s]+:\s+)?(.+?)$", line)
+            if file_name_match:
+                file_name = file_name_match.group(2).strip("/")
+                spoken = map_numbers_to_spoken(file_num)
+                file_map[spoken] = file_name
+                print(f"  {file_num: >2}.   {colour}{line.strip()}{COLOUR_RESET}")
+                file_num += 1
+            else:
+                # Shouldn't ever hit this, means the regexes are missing something
+                print(line)
         else:
-            # Shouldn't ever hit this, means the regexes are missing something
             print(line)
-    else:
-        print(line)
 
-sys.stdout.flush()
+    sys.stdout.flush()
+    return file_map
 
-set_list(GIT_STATUS_ITEMS_CONTEXT, GIT_STATUS_ITEMS_LIST, file_map)
+if __name__ == '__main__':
+    file_map = git_status()
+    set_list(GIT_STATUS_ITEMS_CONTEXT, GIT_STATUS_ITEMS_LIST, file_map)
